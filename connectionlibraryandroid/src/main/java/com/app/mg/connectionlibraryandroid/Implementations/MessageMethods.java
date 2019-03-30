5df23020b2eff8f4cffb913e2a64311b1e16f024
gian franco gabriel caycho <kamigane1998@gmail.com>
2019-03-30 05:46:43 -0500

    modify connect and message methods

--- connectionlibraryandroid/src/main/java/com/app/mg/connectionlibraryandroid/Implementations/MessageMethods.java
+++ connectionlibraryandroid/src/main/java/com/app/mg/connectionlibraryandroid/Implementations/MessageMethods.java
@@ -6,9 +6,10 @@ import com.app.mg.connectionlibraryandroid.Entities.MessageBody;
 import com.google.gson.Gson;
 import com.app.mg.connectionlibraryandroid.Methods.IMessageMethods;
 
+import org.java_websocket.WebSocket;
 import org.java_websocket.client.WebSocketClient;
 
-public class MessageMethods<T,V extends WebSocketClient> implements IMessageMethods<T,V> {
+public class MessageMethods<T,V extends WebSocketClient,W extends WebSocket> implements IMessageMethods<T,V,W> {
     @Override
     public MessageBody ConstructMessageBody(String myIpAddress, T body) {
         ConnectMethods connectMethods = new ConnectMethods();
@@ -18,15 +19,33 @@ public class MessageMethods<T,V extends WebSocketClient> implements IMessageMeth
     }
 
     @Override
+    public String ConstructMessageBodyJSON(String myIpAddress, T body) {
+        Gson gson = new Gson();
+        return gson.toJson(ConstructMessageBody(myIpAddress,body));
+    }
+
+    @Override
     public void SendMessageBody(MessageBody messageBody, V webSocketClient) {
         Gson gson = new Gson();
         webSocketClient.send(gson.toJson(messageBody));
     }
 
     @Override
+    public void SendMessageBody(MessageBody messageBody, W webSocket) {
+        Gson gson = new Gson();
+        webSocket.send(gson.toJson(messageBody));
+    }
+
+    @Override
     public void SendMessageBody(T body, V webSocketClient,String myIpAddress) {
         MessageBody<T> messageBody = ConstructMessageBody(myIpAddress,body);
         SendMessageBody(messageBody,webSocketClient);
+    }
+
+    @Override
+    public void SendMessageBody(T body, W webSocketServer, String myIpAddress) {
+        MessageBody<T> messageBody = ConstructMessageBody(myIpAddress,body);
+        SendMessageBody(messageBody,webSocketServer);
     }
 
     @Override
--- connectionlibraryandroid/src/main/java/com/app/mg/connectionlibraryandroid/Methods/IMessageMethods.java
+++ connectionlibraryandroid/src/main/java/com/app/mg/connectionlibraryandroid/Methods/IMessageMethods.java
@@ -5,15 +5,22 @@ import android.content.Context;
 import com.app.mg.connectionlibraryandroid.Entities.MessageBody;
 import com.app.mg.connectionlibraryandroid.Entities.WebSocketClientImplementation;
 
+import org.java_websocket.WebSocket;
 import org.java_websocket.client.WebSocketClient;
 
 
-public interface IMessageMethods<T,V extends WebSocketClient> {
+public interface IMessageMethods<T,V extends WebSocketClient,W extends WebSocket> {
     MessageBody ConstructMessageBody(String myIpAddress, T body);
+
+    String ConstructMessageBodyJSON(String myIpAddress, T body);
 
     void SendMessageBody(MessageBody messageBody,  V webSocketClient);
 
+    void SendMessageBody(MessageBody messageBody,  W webSocket);
+
     void SendMessageBody(T body,  V webSocketClient,String myIpAddress);
+
+    void SendMessageBody(T body,  W webSocketServer,String myIpAddress);
 
     MessageBody<T> ReceiveMessageBody(String message);
 }
